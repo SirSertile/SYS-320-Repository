f3614e476d87c799f7ef3382cc06a7f73085479d
david.serate <david.serate@cyber.local>
2020-01-17 10:32:39 -0500

    Added 1-17

---
+++ 1-17Class.ps1
@@ -0,0 +1,12 @@
+﻿# David Serate SYS-320
+# List running processes
+# Get-Process | Get-Member 
+# pulling process info 
+# Get-Process | Select-Object ProcessName, ID, Path
+# pull process info via aliases
+#ps | Select ProcessName, ID, Path | export-csv -NoTypeInformation `
+#-Path "C:\Users\david.serate\Desktop\processes.csv"
+# List all registered services
+#Get-Service | where {$_.Status -eq "Running"}
+# List all Running Chrome Processes
+Get-Process | where {$_.ProcessName -eq "Chrome"}

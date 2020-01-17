835b2d9d6f2a16fe382f49964c74d216d093704f
david.serate <david.serate@cyber.local>
2020-01-17 11:29:10 -0500

    Added week1challenge2.ps1

---
+++ Assignments/Week1Challenge2.ps1
@@ -0,0 +1,21 @@
+﻿# David Serate 
+# SYS-320
+# Week 1 Challenge 2 
+# Challenge: Review Security Event Logs
+
+# List all available Event Logs 
+Get-EventLog -list 
+
+# Create user selection prompt - allows for choice of event log
+$logselection = Read-Host -Prompt "Please select a log to review from the list above." 
+
+# Create prompt to let user pick a search string
+$searchstring = Read-Host -Prompt "Please enter a string to filter for in the logs." 
+
+# Define directory 
+$mydir ="C:\Users\david.serate\Desktop"
+
+# Select chosen event log
+Get-EventLog -LogName $logselection -Newest 40 | `
+where {$_.Message -ilike "*$searchstring*"} | `
+Export-Csv -NoTypeInformation -Path "$mydir\MyEventLogs.csv"
